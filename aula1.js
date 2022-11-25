ba011965703f60216084e0ffc60ecb59fa8df613
nathalia123csm <nathaliacristinasmm@gmail.com>
2022-11-25 10:47:50 -0300

    Versão com placar Brasil x Suíça

--- aula1.js
+++ aula1.js
@@ -3,7 +3,7 @@ var dia = require('./biblioteca');
 http.createServer(function (req, res){
     
     res.writeHead(200, {'Content-Type':'text/html'})
-    res.write('<h2>Brasil 2 x 0 Servia <h2>');
+    res.write('<h2>Brasil 2 x 1 Suíça <h2>');
     res.write('<p style=color:purple>'+Date().substring(16, 24)+'<br>');
     res.write(dia.diaDaSemana()+'</p><br>')
     res.end('Bom dia!');
