943617bf42f8c5e40fa950dbf10f45a789ef29dc
nathalia123csm <nathaliacristinasmm@gmail.com>
2022-11-25 10:43:49 -0300

    Versão 3 - Biblioteca com função dia da semana colorido

--- aula1.js
+++ aula1.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var dia = require('./biblioteca');
 http.createServer(function (req, res){
     
     res.writeHead(200, {'Content-Type':'text/html'})
     res.write('<h2>Brasil 2 x 0 Servia <h2>');
-    res.write(Date().substring(16, 24)+'<br>');
+    res.write('<p style=color:purple>'+Date().substring(16, 24)+'<br>');
+    res.write(dia.diaDaSemana()+'</p><br>')
     res.end('Bom dia!');
 
 }).listen(3000);
